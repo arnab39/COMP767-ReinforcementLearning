2fcf390002c14579a0730365da33eb6124c25bdd
Arna Ghosh <arnatubaikgp@gmail.com>
2020-02-03 02:33:33 -0500

    adding the gridworld class and updated plotting functions

--- Assignment1/MDP/ExampleGridClass.py
+++ Assignment1/MDP/ExampleGridClass.py
@@ -3,59 +3,51 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import gym
+import emdp.gridworld as gw
 
-class FrozenLakeClass():
-	def __init__(self,name='FrozenLake-v0',slippery=False,seed=13,gamma=0.9):
-		self.env = gym.make(name,is_slippery=slippery)
-		self.env.seed(seed)
-		self.env.reset()
-		self.obs = self.env.observation_space
-		self.actions = self.env.action_space
-		self.num_states = self.obs.n
-		self.num_actions = self.actions.n
-		self.transition, self.rewards = self.transition_reward_matrix()
+class ExampleGridClass():
+	def __init__(self, seed, grid_size=5, gamma=0.9):
+		self.grid_size = grid_size
+		self.seed = seed
+		np.random.seed(self.seed)
+		self.terminal_states = [(self.grid_size-1,self.grid_size-1)]
+		self.transition = gw.build_simple_grid(self.grid_size,self.terminal_states, p_success=1)
+		self.rewards = np.zeros((self.transition.shape[0], self.transition.shape[1]))
+		self.start_prob = np.zeros(self.transition.shape[0]) 
 		self.gamma = gamma
-		np.random.seed(seed)
+		self.num_states = self.transition.shape[0]
+		self.num_actions = self.transition.shape[1]
+		self.set_transition_and_reward()
+		self.grid = gw.GridWorldMDP(self.transition, self.rewards, self.gamma, self.start_prob, self.terminal_states, self.grid_size, self.seed)
 		self.max_T = self.num_states*3
-
-	def valid_action(self, a):
-		assert self.actions.contains(a), "Invalid action chosen!!"
-
-	def valid_state(self, s):
-		assert self.obs.contains(s), "Invalid state reached!!"
-
-	def transition_reward_matrix(self):
-		P = np.zeros((self.num_states,self.num_actions,self.num_states))
-		R = np.zeros((self.num_states,self.num_actions,self.num_states))
-		for s in range(self.num_states):
-			for a in range(self.num_actions):
-				for entry in self.env.P[s][a]:
-					P[s][a][entry[1]] = entry[0]
-					R[s][a][entry[1]] = entry[2]
-
-		return P,R
-
+ 	
+	def set_transition_and_reward(self):
+		self.start_prob[np.random.randint(low=0,high=self.num_states-1)] = 1		
+		self.transition[6,:,:] = 0
+		self.transition[6,:,21] = 1
+		self.rewards[6, 3] = +2
+		self.rewards[(self.grid_size-1)*self.grid_size-1, 3] = +10
+		self.rewards[self.grid_size*self.grid_size-2, 1] = +1
+		return 
+	
 	def run_episode(self, policy):
+		self.grid.reset()
 		attempts = 1
 		cumulative_reward = 0
 		total_steps = 0
-		observation = self.env.reset()
+		state = self.grid.current_state.argmax()
 		for t in range(self.max_T):
-			prob_action = np.cumsum(policy[observation])
+			prob_action = np.cumsum(policy[state])
 			p = np.random.uniform(0,1)
 			action = np.where(prob_action>p)[0][0]
-			observation, reward, done, info = self.env.step(action)
+			state, reward, done, _ = self.grid.step(int(action))
+			state = state.argmax()
 			cumulative_reward += (self.gamma**t)*reward
 			total_steps += 1
-			if done and reward==1:
+			if done:
 				break
-			elif done and reward==0:
-				attempts += 1
-				total_steps += self.max_T
-				observation = self.env.reset()
 
 		return cumulative_reward, total_steps
-
 
 def greedify_policy(Q_vals):
 	tolerance = 1e-9
@@ -331,7 +323,7 @@ def plot_performance_value_iteration(env_class,name='FrozenLake-v0', iterations=
 
 
 if __name__ == '__main__':
-	# policy_iteration(FrozenLakeClass('FrozenLake8x8-v0',seed=13))
-	# plot_performance_policy_iteration(env_class=FrozenLakeClass, name='FrozenLake-v0')
-	# value_iteration(FrozenLakeClass('FrozenLake-v0',seed=13),gamma=0.9)
-	plot_performance_value_iteration(env_class=FrozenLakeClass, name='FrozenLake-v0')
+	# policy_iteration(environment=ExampleGridClass(seed=1))
+	# plot_performance_policy_iteration(env_class=ExampleGridClass)
+	# value_iteration(ExampleGridClass(seed=1))
+	plot_performance_value_iteration(env_class=ExampleGridClass)
