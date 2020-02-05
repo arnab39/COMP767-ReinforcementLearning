77da0c4638c6679ce16622daab8259d59d393cb0
Arna Ghosh <arnatubaikgp@gmail.com>
2020-02-05 06:09:27 +0530

    Adding ExampleGridClass commands

--- Assignment1/MDP/Experiment.py
+++ Assignment1/MDP/Experiment.py
@@ -4,8 +4,11 @@ from FrozenLake import FrozenLakeClass
 from Grid import ExampleGridClass
 
 if __name__ == '__main__':
-	#PI.policy_iteration(environment=FrozenLakeClass(name='FrozenLake-v0',seed=1))
-	#PI.plot_performance_policy_iteration(env_class=FrozenLakeClass, name = "FrozenLake8x8-v0")
+	# PI.policy_iteration(environment=FrozenLakeClass(name='FrozenLake-v0',seed=1))
+	# PI.plot_performance_policy_iteration(env_class=FrozenLakeClass, name = "FrozenLake8x8-v0")
 	# VI.value_iteration(FrozenLakeClass('FrozenLake-v0',seed=13))
-	VI.plot_performance_value_iteration(env_class=FrozenLakeClass, name = "FrozenLake8x8-v0")
-
+	# VI.plot_performance_value_iteration(env_class=FrozenLakeClass, name = "FrozenLake8x8-v0")
+	# PI.policy_iteration(environment=ExampleGridClass(seed=1))
+	# PI.plot_performance_policy_iteration(env_class=ExampleGridClass)
+	# VI.value_iteration(ExampleGridClass(seed=1))
+	VI.plot_performance_value_iteration(env_class=ExampleGridClass)
